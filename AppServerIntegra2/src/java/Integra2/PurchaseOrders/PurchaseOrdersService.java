6b7c0ac60f3967bc60ef9cc79627a63f9148a77d
Guillermo Valenzuela <gevalenz@gmail.com>
2015-05-22 01:56:37 -0300

    URL fix. Get purchase order method implemented

--- AppServerIntegra2/src/java/Integra2/PurchaseOrders/PurchaseOrdersService.java
+++ AppServerIntegra2/src/java/Integra2/PurchaseOrders/PurchaseOrdersService.java
@@ -6,11 +6,21 @@
 package Integra2.PurchaseOrders;
 
 import Integra2.BasicUrls;
-
+import Integra2.HttpRequest;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 /**
  *
  * @author Guillermo
  */
+@Path("PurchaseOrders")
 public class PurchaseOrdersService {
     
     //
@@ -20,4 +30,14 @@ public class PurchaseOrdersService {
     private static final String CANCEL_ORDER_URL = BasicUrls.PURCHASE_ORDER_SERVER + "/anular/"; //DELETE
     private static final String GET_ORDER_URL = BasicUrls.PURCHASE_ORDER_SERVER + "/obtener/"; //GET
     //private static final String DISPATCH_ORDER_URL = BasicUrls.PURCHASE_ORDER_SERVER + "";
+    
+    
+    @GET
+    @Produces("application/json")
+    @Path("/{id}")
+    public String getOrder(@PathParam("id") String orderId){
+        
+        HttpRequest response = HttpRequest.get(GET_ORDER_URL + orderId).contentType("application/json");
+        return response.body();
+    }
 }
