d5dbd0ce0acc09327ad77ff552cc92513ccbe2f1
Guillermo Valenzuela <gevalenz@gmail.com>
2015-05-22 00:04:34 -0300

    Basic project structure

---
+++ AppServerIntegra2/src/java/Integra2/Odoo/OdooService.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Integra2.Odoo;
+
+/**
+ *
+ * @author Guillermo
+ */
+public class OdooService {
+    
+}
