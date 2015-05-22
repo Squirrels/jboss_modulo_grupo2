9f99fded02e7feee4ca36e218138307463218c54
Guillermo Valenzuela <gevalenz@gmail.com>
2015-05-22 01:01:30 -0300

    Added urls

--- AppServerIntegra2/src/java/Integra2/PurchaseOrders/PurchaseOrdersService.java
+++ AppServerIntegra2/src/java/Integra2/PurchaseOrders/PurchaseOrdersService.java
@@ -5,10 +5,19 @@
  */
 package Integra2.PurchaseOrders;
 
+import Integra2.BasicUrls;
+
 /**
  *
  * @author Guillermo
  */
 public class PurchaseOrdersService {
     
+    //
+    private static final String PUT_ORDER_URL = BasicUrls.PURCHASE_ORDER_SERVER + "/crear"; //PUT
+    private static final String ACCEPT_ORDER_URL = BasicUrls.PURCHASE_ORDER_SERVER + "/recepcionar/"; //POST
+    private static final String REJECT_ORDER_URL = BasicUrls.PURCHASE_ORDER_SERVER + "/rechazar/"; //POST
+    private static final String CANCEL_ORDER_URL = BasicUrls.PURCHASE_ORDER_SERVER + "/anular/"; //DELETE
+    private static final String GET_ORDER_URL = BasicUrls.PURCHASE_ORDER_SERVER + "/obtener/"; //GET
+    //private static final String DISPATCH_ORDER_URL = BasicUrls.PURCHASE_ORDER_SERVER + "";
 }
