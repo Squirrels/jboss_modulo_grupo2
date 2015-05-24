ecac70d30c6b42316db61804bc8f05ceef1400ad
jnmatte <jnmatte@gmail.com>
2015-05-24 12:28:16 -0300

    GetCartola v1. Fix path getTransactions

---
+++ AppServerIntegra2/src/main/java/Integra2/Bank/ClassGetCartola.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Integra2.Bank;
+
+/**
+ *
+ * @author jamatte
+ */
+public class ClassGetCartola {
+    public float inicio;
+    public float fin;
+    public String id;
+    public int limit;
+    
+    
+    public ClassGetCartola(float inicio, float fin, String id, int limit){
+        this.inicio = inicio;
+        this.fin = fin;
+        this.id = id;
+        this.limit = limit;
+    }
+}
