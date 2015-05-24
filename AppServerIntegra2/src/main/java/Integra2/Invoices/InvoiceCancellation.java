397df982b11b74d01603d43fda6f21eb1aa1ece8
afmartinez <andresmartinezro@gmail.com>
2015-05-23 21:21:44 -0300

    Invoice v1

---
+++ AppServerIntegra2/src/main/java/Integra2/Invoices/InvoiceCancellation.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Integra2.Invoices;
+
+/**
+ *
+ * @author Andres
+ */
+public class InvoiceCancellation {
+    
+    public String anulacion;
+    
+    public InvoiceCancellation(){}
+    
+    public InvoiceCancellation(String anulacion){
+        this.anulacion = anulacion;
+    }
+}
---
+++ AppServerIntegra2/src/main/java/Integra2/Invoices/InvoiceRejection.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Integra2.Invoices;
+
+/**
+ *
+ * @author Andres
+ */
+public class InvoiceRejection {
+    public String rechazo;
+    
+    public InvoiceRejection() {};
+    
+    public InvoiceRejection(String rechazo){
+        this.rechazo = rechazo;
+    }
+}
