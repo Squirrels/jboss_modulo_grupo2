fc059180cabcd99820bbfab16b5fbb991feb9d30
Guillermo Valenzuela <gevalenz@gmail.com>
2015-05-24 18:39:54 -0300

    getTransaction v1, createTransaction response updated, getSaldo response updated

--- AppServerIntegra2/src/main/java/Integra2/Bank/BankResource.java
+++ AppServerIntegra2/src/main/java/Integra2/Bank/BankResource.java
@@ -15,6 +15,7 @@ import Integra2.Bank.Account.ErrorException;
 import Integra2.Bank.Account.GetCartola;
 import Integra2.Bank.Transaction.CrearTransaccion;
 import Integra2.Bank.Transaction.Transaccion;
+import Integra2.Bank.Transaction.TransaccionArray;
 import Integra2.Bank.Transaction.Trx;
 import Integra2.Bank.Transaction.Trx_Service;
 import java.util.List;
@@ -36,7 +37,7 @@ public class BankResource {
     
     @GET
     @Produces("application/json")
-    @Path("/{id}")
+    @Path("/Account/{id}")
     public Response getAccount(@PathParam("id") String accountId) throws BadParametersException, ErrorException{
         
         try { // Call Web Service Operation
@@ -47,7 +48,12 @@ public class BankResource {
             CuentaBancoArray result = port.getCuenta(accountId);
             List<CuentaBanco> accountList = result.getItem();
             CuentaBanco bankAccount = accountList.get(0);
-            String json = "{\"saldo\":" + bankAccount.getSaldo() + "}";
+            String id = bankAccount.getId();
+            String group = bankAccount.getGrupo();
+            int balance = bankAccount.getSaldo();
+            String json = "{\"id\":\"" + id + "\","
+                    + "\"grupo\":\"" + group + "\","
+                    + "\"saldo\":" + balance + "}";
             return Response.ok(json, MediaType.APPLICATION_JSON).build();
         } catch (Exception ex) {
             // TODO handle custom exceptions here
@@ -58,7 +64,7 @@ public class BankResource {
 
     @POST
     @Produces("application/json")
-    @Path("/cartola")
+    @Path("/AccountStatement/")
     public Response getCartola(ClassGetCartola CgetCartola) throws BadParametersException, ErrorException{
         
         try { // Call Web Service Operation
@@ -73,13 +79,14 @@ public class BankResource {
 
             // TODO process result here
             Cartola result = port.getCartola(cartola);
+            
             return Response.ok("Result = " + result, MediaType.TEXT_PLAIN).build();
             
         } catch (Exception ex) {
             // TODO handle custom exceptions here
+            String json = "{\"error\":\"" + ex.toString() + "\"}";
+            return Response.status(501).tag(json).type(MediaType.APPLICATION_JSON).build();
         }
-        
-        return Response.status(501).build();
         
     }
     
@@ -101,15 +108,62 @@ public class BankResource {
             
             // TODO process result here
             Transaccion result = port.crearTransaccion(trx);
-            return Response.ok("Result = " + result, MediaType.TEXT_PLAIN).build();
+            
+            String id = result.getId();
+            String from = result.getOrigen();
+            String to = result.getDestino();
+            int amount = result.getMonto();
+            String date = result.getUpdatedAt();
+            String v = result.getV();
+            String json = "{\"id\":\"" + id + "\","
+                    + "\"origen\":\"" + from + "\","
+                    + "\"destino\":\"" + to + "\","
+                    + "\"monto\":" + amount + ","
+                    + "\"updatedAt\":\"" + date + "\","
+                    + "\"v\":\"" + v + "\""
+                    +"}";
+            
+            return Response.ok(json, MediaType.APPLICATION_JSON).build();
             
         } catch (Exception ex) {
             // TODO handle custom exceptions here
-        }
-        
-        return Response.status(501).build();
-
+            String json = "{\"error\":\"" + ex.toString() + "\"}";
+            return Response.status(501).tag(json).type(MediaType.APPLICATION_JSON).build();
+        }           
+    }
     
+    @GET
+    @Produces("application/json")
+    @Path("/Transactions/{id}")
+    public Response getTransaction(@PathParam("id")String transactionId){
         
-}
+        try { // Call Web Service Operation
+            Trx_Service service = new Trx_Service();
+            Trx port = service.getTrxPort();
+            // TODO process result here
+            TransaccionArray result = port.getTransaccion(transactionId);
+            Transaccion transaction = result.getItem().get(0);
+            
+            String id = transaction.getId();
+            String from = transaction.getOrigen();
+            String to = transaction.getDestino();
+            int amount = transaction.getMonto();
+            String date = transaction.getUpdatedAt();
+            String v = transaction.getV();
+            String json = "{\"id\":\"" + id + "\","
+                    + "\"origen\":\"" + from + "\","
+                    + "\"destino\":\"" + to + "\","
+                    + "\"monto\":" + amount + ","
+                    + "\"updatedAt\":\"" + date + "\","
+                    + "\"v\":\"" + v + "\""
+                    +"}";
+            
+            return Response.ok(json, MediaType.APPLICATION_JSON).build();
+        } catch (Exception ex) {
+            // TODO handle custom exceptions here
+            String json = "{\"error\":\"" + ex.toString() + "\"}";
+            return Response.status(501).tag(json).type(MediaType.APPLICATION_JSON).build();
+        }
+
+    }
 }
